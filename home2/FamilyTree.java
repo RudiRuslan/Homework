71c45fe8b3831c5654ad595ece1fd21c0f0ea143
Ruslan <113286815+RudiRuslan@users.noreply.github.com>
2023-02-20 21:54:13 +0300

    Create FamilyTree.java

---
+++ home2/FamilyTree.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+/**
+* этот класс генеалогического древа действует как реализация класса членов семейства
+ */
+public class FamilyTree implements Serializable{
+    //необходимо для управления версиями сериализованных файлов
+    private static final long serialVersionUID = 1;
+
+    /**
+     * строит генеалогическое древо, устанавливая корень равным нулю
+     */
+    public FamilyTree() {
+        this.root = null;        
+    }
+    
+    private FamilyMember root;
+    
+    /**
+     * устанавливает корень
+     * параметр новый корень
+     */
+    public void setRoot(FamilyMember newRoot){
+        this.root = newRoot;
+    }
+
+    public boolean hasRoot(){
+        return this.root !=null;
+    }
+    
+    public FamilyMember getRoot(){
+        return this.root;
+    }
+}
